70c2d371b022163d71124c1feb75bfc4c9a73700
Tu Huynh <tu.huynh@linecorp.com>
2020-01-13 14:33:29 +0700

    temp disable worker

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 require("app-module-path/register");
 require("dotenv").config();
 
-const { isMainThread } = require("worker_threads");
+// const { isMainThread } = require("worker_threads");
 const app = require("server/app");
 const initDB = require("configs/mongodb").initDB;
 const cron = require("node-cron");
@@ -13,22 +13,24 @@ const main = async () => {
     const port = process.env.PORT || 5000;
     server.listen(port, () => console.log(`Server listening on port ${port}!`));
 
-    const remoteWorker = require("workers/remote");
-    remoteWorker(__filename);
+    // const remoteWorker = require("workers/remote");
+    // remoteWorker(__filename);
 
     // Every hour
-    cron.schedule("0 * * * *", () => {
-      remoteWorker(__filename);
-    });
+    // cron.schedule("0 * * * *", () => {
+    //   remoteWorker(__filename);
+    // });
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
 };
 
-if (isMainThread) {
-  main();
-} else {
-  const spawnWorker = require("workers/worker");
-  spawnWorker();
-}
+main();
+
+// if (isMainThread) {
+//   main();
+// } else {
+//   const spawnWorker = require("workers/worker");
+//   spawnWorker();
+// }
